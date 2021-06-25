61e1f6e6d1ee44aac8f216da3078bbb1156224eb
said <said@said.said>
2021-06-26 00:15:57 +0200

    added SectionList with mock

--- components/Items.js
+++ components/Items.js
@@ -29,7 +29,7 @@ export default function Items(props){
                 >
                 <ListItem.Content >
                     <ListItem.Title onPress={()=>setFullLabel(!fullLabel)}>
-                        {fullLabel ? label : label.length < 25 ? `${label}` : `${label.substring(0, 25)}...`}
+                        {fullLabel ? label : label?.length < 25 ? `${label}` : `${label.substring(0, 25)}...`}
                     </ListItem.Title>
                 </ListItem.Content>
                 
--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,27 +1,55 @@
 import React from 'react'
 import {
   View,
+  Text,
+  SectionList,
   StyleSheet } from 'react-native'
+import COLORS from '../constants/theme'
 import Items from '../components/Items'
-
 
 //SCREEN
 export default function MainScreen(){
+    const DATA = [
+        {
+        title: "A",
+        data: ["PizzaPizzaPizzaPizzaPizzaPizzaPizzaPizzaPizzaPizzaPizza", "Burger", "Risotto"]
+        },
+        {
+        title: "B",
+        data: ["French Fries", "Onion Rings", "Fried Shrimps"]
+        },
+        {
+        title: "C",
+        data: ["Water", "Coke", "Beer"]
+        },
+        {
+        title: "D",
+        data: ["Cheese Cake", "Ice Cream"]
+        }
+    ];
 
-
-  return(
+    return(
         <View style={styles.container}>
-            <Items label={"Mock 1"} onPress={()=>alert('Modifier mock1')}/>
-            <Items label={"Mock 2 Mock 2 Mock 2 Mock 2 Mock 2 Mock 2 Mock 2 Mock 2 Mock 2 "} onPress={()=>alert('Modifier mock2')}/>
+            <SectionList
+                sections={DATA}
+                keyExtractor={(item, index) => item + index}
+                renderItem={({ item }) => <Items label={item} onPress={()=>alert(item)}/>}
+                renderSectionHeader={({ section: { title } }) => (
+                    <Text style={styles.sectionTitleContent}>{title}</Text>
+                )}
+                />
         </View>
     )
 }
 
 //STYLE
 const styles = StyleSheet.create({
-  container: {
-    flex:1,
-    alignItems:'center',
-    justifyContent:'center'
-  }
+    container: {
+        flex:1,
+        alignItems:'center'
+    },
+    sectionTitleContent: {
+        fontSize: 24,
+        color:COLORS.SECONDARY
+    }
 })
